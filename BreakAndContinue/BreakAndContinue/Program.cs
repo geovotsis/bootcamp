1447c4ec39eecae19bf7b6b0244b7c92f905ef5c
geovotsis <62116418+geovotsis@users.noreply.github.com>
2020-03-12 21:26:27 +0200

    Add files via upload

---
+++ BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                if (i > 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("----------------------");
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i <= 5)
+                {
+                    continue;
+
+                }
+                Console.WriteLine(i);
+            }    
+
+        }
+    }
+}
